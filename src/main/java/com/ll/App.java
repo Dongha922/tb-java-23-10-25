055bd2904c1dbd46d4635fbc6d8c264c043c408b
dongha922 <kdh4575@naver.com>
2023-10-25 15:12:59 +0900

    스캐너 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,17 @@
 package com.ll;
 
+import java.util.Scanner;
+
 class App{
     void run(){
         System.out.println("프로그램 실행");
+        System.out.println("===명언앱 ===");
+        System.out.printf("명령) ");
+
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+
+        System.out.print("입력받은 명령어 :" + cmd + "\n");
     }
+
 }
