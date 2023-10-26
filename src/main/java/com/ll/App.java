80aa1fe0fad4968d2457a92e410cedc2726b5a70
dongha922 <kdh4575@naver.com>
2023-10-26 10:09:55 +0900

    등ë¡세팅

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,14 +4,24 @@ import java.util.Scanner;
 
 class App{
     void run(){
-        System.out.println("프로그램 실행");
+
         System.out.println("===명언앱 ===");
-        System.out.printf("명령) ");
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
 
-        System.out.print("입력받은 명령어 :" + cmd + "\n");
+        while (true){
+            System.out.printf("명령) ");
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+            if(cmd .equals("종료")) {
+                break;
+            }
+            else if(cmd .equals("등록")){
+                System.out.println("명언 : ");
+                System.out.println("작가 : ");
+
+           }
+        }
     }
+
 
 }
