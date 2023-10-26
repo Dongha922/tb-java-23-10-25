f87892eec317893eacdf71d626facf8bdae78eb3
dongha922 <kdh4575@naver.com>
2023-10-26 13:07:08 +0900

    목록기능 오류

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 class App {
     void run() {
         int index = 0;
-        //String title = "제목", author = "작가";
+        //배열생성
+        String[] titleArr = new String[5];
+        String[] authorArr = new String[6];
+        //프로그램시작
         System.out.println("===명언앱 ===");
 
 
@@ -17,23 +20,21 @@ class App {
                 break;
             } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                //Scanner scanner1 = new Scanner(System.in);
-                String title = scanner.nextLine();
-
+                titleArr[index]= scanner.nextLine();
                 System.out.print("작가 : ");
-                //Scanner scanner2 = new Scanner(System.in);
-                String author = scanner.nextLine();
+                authorArr[index]= scanner.nextLine();
+
+
+                System.out.println(titleArr[index]);
+                System.out.println(authorArr[index]);
                 index++;
-
-                System.out.println(title);
-                System.out.println(author);
                 System.out.println(index + "번 명언이 등록되었습니다.");
-
-
             } else if (cmd.equals("목록")) {
                 System.out.println(" 번호 / 작가 / 명언 ");
                 System.out.println("------------------------");
-                //System.out.println(index + "/" + author + "/" + title);
+                for (int i = 0; i < titleArr.length; i++) {
+                    System.out.println((i+1) +" / " + titleArr[i]+ " / " +authorArr[i]);
+                }
             }
         }
     }
